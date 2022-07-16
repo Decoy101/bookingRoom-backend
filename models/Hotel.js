1fc63122363c40ad40a1ae8a2c463b8ab6119945
Aman Gupta <00amangpt00@gmail.com>
2022-07-16 21:06:32 +0530

    lol

---
+++ models/Hotel.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const HotelSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+        
+    },
+    city:{
+        type: String,
+        required: true
+    },
+    
+});
+
+export default mongoose.model("Hotel", HotelSchema)
